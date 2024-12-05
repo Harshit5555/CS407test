68df7806920987daf8fc7a1010ad2459d4aca40d
Harshit Joshi <hjoshi4@wisc.edu>
2024-12-05 15:05:09 -0600

    Initial Commit

---
+++ HelloCS407/HelloCS407App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCS407App.swift
+//  HelloCS407
+//
+//  Created by Harshit joshi on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloCS407App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloCS407Tests/HelloCS407Tests.swift
@@ -0,0 +1,16 @@
+//
+//  HelloCS407Tests.swift
+//  HelloCS407Tests
+//
+//  Created by Harshit joshi on 05/12/24.
+//
+
+import Testing
+
+struct HelloCS407Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
